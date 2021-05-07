08a2a30ffe0e9bea16f084ea13afd8df472037a6
PriyankaMullai <nanthinimppm@gmail.com>
2021-05-07 06:01:26 -0700

    own

---
+++ src/org/sample/Test1.java
@@ -0,0 +1,18 @@
+package org.sample;
+
+public class Test1 {
+	private void ts() {
+		System.out.println("ts1");
+	}
+
+	private void tcc() {
+		System.out.println("tdsg");
+
+	}
+
+	public static void main(String[] args) {
+		Test1 t = new Test1();
+		t.tcc();
+		t.ts();
+	}
+}
